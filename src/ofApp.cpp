d8baa1069c57e3fbed7b8295142a80d657b442d0
Collin Schupman <collinschupman@gmail.com>
2016-11-04 10:49:12 -0700

    blah

---
+++ src/ofApp.cpp
@@ -0,0 +1,34 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+	ofSetWindowTitle("ofxSpout receiver example");
+
+	// initialize Spout as a receiver
+	ofxSpout::init("", receiver, ofGetWidth(), ofGetHeight(), false);
+}
+
+void ofApp::update() {
+	// init receiver if it's not already initialized
+	ofxSpout::initReceiver(receiver);
+
+	// receive Spout texture
+	ofxSpout::receiveTexture(receiver);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+
+	receiver.draw(0, 0);
+
+	ofDrawBitmapStringHighlight("ofxSpout receiver example", ofGetWidth() - 220, ofGetHeight() - 20);
+}
+
+//--------------------------------------------------------------
+void ofApp::exit()
+{
+	// exit spout
+	ofxSpout::exit();
+}
---
+++ src/ofApp.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxSpout.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();	
+		void exit();
+
+	private:
+		ofTexture receiver;
+};
